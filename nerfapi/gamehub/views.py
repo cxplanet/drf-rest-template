2610e73d1d6b95ff5151c20863856e08c96c5858
JayLVulcan <jayl@vulcan.com>
2020-03-07 22:51:45 -0800

    simple player and team endpoints

--- nerfapi/gamehub/views.py
+++ nerfapi/gamehub/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from . import models
+from . import serializers
+
+# Create your views here.
 class StatusView(APIView):
     def get(self, request):
         content = {'status': 'ok'}
         return Response(content)
+
+class PlayerViewset(viewsets.ModelViewSet):
+    queryset = models.Player.objects.all()
+    serializer_class = serializers.PlayerSerializer
+
+class TeamViewset(viewsets.ModelViewSet):
+    queryset = models.Belonging.objects.all()
+    serializer_class = serializers.TeamSerializer
+
